965b8868c2c66a7fb3543638b0cd591065a1cb5b
muon-rw <pashah012@gmail.com>
2024-09-08 13:08:29 -0500

    Scorched Guns optional

--- src/main/java/dev/davey/apotheosis_ascended/AALootCategories.java
+++ src/main/java/dev/davey/apotheosis_ascended/AALootCategories.java
@@ -3,19 +3,27 @@ package dev.davey.apotheosis_ascended;
 import dev.shadowsoffire.apotheosis.adventure.loot.LootCategory;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.ItemStack;
-import top.ribs.scguns.item.GunItem;
-
+import net.minecraftforge.fml.ModList;
 
 public class AALootCategories {
 
-    public static final LootCategory GUN = LootCategory.register(LootCategory.SWORD, "gun",
-            s -> s.getItem() instanceof GunItem, arr(EquipmentSlot.MAINHAND));
+    public static final LootCategory GUN;
+
+    static {
+        if (ModList.get().isLoaded("scguns")) {
+            GUN = LootCategory.register(LootCategory.SWORD, "gun",
+                    s -> s.getItem() instanceof top.ribs.scguns.item.GunItem, arr(EquipmentSlot.MAINHAND));
+        } else {
+            GUN = null;
+        }
+    }
+
     private static EquipmentSlot[] arr(EquipmentSlot... s) {
         return s;
     }
 
     public static boolean isGun(ItemStack stack) {
-        return LootCategory.forItem(stack).equals(GUN);
+        return GUN != null && LootCategory.forItem(stack).equals(GUN);
     }
 
     public static void init() {}
